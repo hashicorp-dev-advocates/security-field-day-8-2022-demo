05da5923d8c392da7db16aedaea5459b5c5893fe
Robert Barnes <robert.raybarnes@gmail.com>
2023-06-06 18:26:55 +0200

    updating blueprint to include database table

--- shipyard/config/postgres/files/psql-setup.sh
+++ shipyard/config/postgres/files/psql-setup.sh
@@ -2,3 +2,13 @@
 
 psql "host=localhost port=5432 dbname=profiles user=postgres password=postgres sslmode=disable" \
   -c "CREATE USER vaultuser WITH SUPERUSER PASSWORD 'vaultpass';"
+
+psql "host=localhost port=5432 dbname=profiles user=postgres password=postgres sslmode=disable" \
+  -c 'CREATE TABLE IF NOT EXISTS profiles (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR (255) NOT NULL,
+        first_name VARCHAR (255) NOT NULL,
+        surname VARCHAR (255) NOT NULL,
+        email VARCHAR (255) UNIQUE NOT NULL,
+        password VARCHAR (255) NOT NULL
+);'
